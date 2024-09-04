b7b4d101ff02f3b3561a3eefddcd7b59e2bffffe
bachsofttrick <xuanbach1307@gmail.com>
2024-09-03 18:22:14 -0700

    no jekyll

--- next.config.js
+++ next.config.js
@@ -2,16 +2,8 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  //output: 'export',
- 
-  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-  // trailingSlash: true,
- 
-  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-  // skipTrailingSlashRedirect: true,
- 
-  // Optional: Change the output directory `out` -> `dist`
-  //distDir: 'build',
+  output: 'export',
+  distDir: 'build',
 }
  
 module.exports = nextConfig
