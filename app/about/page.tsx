e1a0a7bb69164fdfa1e6cc4ba6a1dc1ff2e5b74e
bachsofttrick <xuanbach1307@gmail.com>
2024-09-08 22:42:16 -0700

    add menu to about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -13,8 +13,42 @@ export default function Page() {
     notFound()
   }
 
+  const navItems = [
+    {
+      name: 'Work Experience',
+      route: 'work-experience'
+    },
+    {
+      name: 'Projects',
+      route: 'projects'
+    },
+    {
+      name: 'Skills',
+      route: 'skills'
+    },
+    {
+      name: 'Education',
+      route: 'education'
+    },
+    {
+      name: 'References',
+      route: 'references'
+    },
+    
+  ]
+
   return (
     <section>
+      <nav>
+        <ul className='flex flex-col md:flex-row justify-center items-center'>
+          <li className='md:me-8 font-bold'>Jump to:</li>
+          {
+            navItems.map((item) => (
+              <li key={item.route}><a href={'#'+item.route} className='md:me-8'>{item.name}</a></li>
+            ))
+          }
+        </ul>
+      </nav>
       <article className="prose">
         <CustomMDX source={post[0].content} type='resume' />
       </article>
