037ec15534a3e684187beeea3d05162c43dd3fd6
bachsofttrick <xuanbach1307@gmail.com>
2025-11-28 23:05:36 -0800

    forgot "Phone: "

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -47,7 +47,7 @@ export default function Footer() {
     <footer className="mb-16">
       <ul className="font-sm mt-8 flex flex-col space-x-0 space-y-2 text-neutral-600 md:flex-row md:space-x-4 md:space-y-0 dark:text-neutral-300">
         <FooterPart title={contactInfo.address} noArrow />
-        <FooterPart title={contactInfo.phone} noArrow />
+        <FooterPart title={`Phone: ${contactInfo.phone}`} noArrow />
       </ul>
       <ul className="font-sm flex flex-col space-x-0 space-y-2 text-neutral-600 md:flex-row md:space-x-4 md:space-y-0 dark:text-neutral-300">
         <FooterPart href={contactInfo.github} title={"github"} />
